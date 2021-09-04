7e1bd923fa360cfc8359cdffdad3c752da23d75d
Dimitri Denisjonok <dimitri.781@gmail.com>
2021-09-04 14:50:44 +0100

    ch02-repository-pattern

---
+++ repository.py
@@ -0,0 +1,57 @@
+import abc
+
+import model
+
+
+class AbstractRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, batch: model.Batch):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, reference) -> model.Batch:
+        raise NotImplementedError
+
+
+class FakeRepository(AbstractRepository):
+
+    def __init__(self, batches):
+        self._batches = set(batches)
+
+    def add(self, batch):
+        self._batches.add(batch)
+
+    def get(self, reference):
+        return next(b for b in self._batches if b.reference == reference)
+
+    def list(self):
+        return list(self._batches)
+
+
+class SqlAlchemyRepository(AbstractRepository):
+
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch):
+        self.session.add(batch)
+
+    def get(self, reference):
+        return self.session.query(model.Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(model.Batch).all()
+
+
+class SqlRepository(AbstractRepository):
+    # Tests: https://github.com/cosmicpython/code/blob/chapter_02_repository_exercise/test_repository.py
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch):
+        # self.session.execute('INSERT INTO ??
+        ...
+
+    def get(self, reference) -> model.Batch:
+        # self.session.execute('SELECT ??
+        ...
